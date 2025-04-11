886188c27eb32a508db3789f84be4e963feace2f
Sebastian Ernesto Cordova Orellana <sebastiancordova562@gmail.com>
2025-04-11 13:27:04 -0600

    Actualizacion

--- js/externals.js
+++ js/externals.js
@@ -52,59 +52,151 @@ async function recordVideo() {
 
     }
 }
+
 function geolocalizacion(){
-     if ( navigator.permissions && navigator.permissions.query) {
-        navigator.permissions.query({name:`geolocation`}).then(function(result){
-            const permissions = result.state;
-            if(permissions === `granted` || permissions === `prompt`){
-                _onGetCurrentlocation();
+    if(navigator.permissions && navigator.permissions.query){
+        navigator.permissions.query({name:'geolocation'}).then(function(result){
+            const permission = result.state;
+            if(permission === 'granted'|| permission === 'prompt'){
+                _onGetCurrentLocation();
             }
+            
         });
-        
-     }else if(navigator.geolocation){
-        _onGetCurrentlocation();
-     }
+    }else if(navigator.geolocation){
+        _onGetCurrentLocation();
+    }
 }
 
-function _onGetCurrentlocation(){
+
+function _onGetCurrentLocation(){
     const options = {
         enableHighAccuracy: true,
-        Timeout: 5000,
+        timeout: 5000,
         maximumAge: 0
     };
     navigator.geolocation.getCurrentPosition(function(position){
         const marker = {
             lat: position.coords.latitude,
-            lgn: position.coords.latitude
+            lng: position.coords.longitude
         };
         let enlace = document.getElementById("ir_mapa");
-        enlace.href= `https://maps.google.com/?q=${marker.lat},${marker.lng}`;
-        enlace.text ="IR AL MAPA";
+        enlace.href = `https://maps.google.com/?q=${marker.lat},${marker.lng}`;
+        enlace.text = "IR AL MAPA";
         enlace.target = "_blank";
 
-    },function(error){
+    }, function(error){
         console.log(error);
-
-    },options);
+    },options); 
 }
 
 
 const init = () =>{
-    const tieneSoprteUsermedia = () =>
-        ||(navigator.mediaDevices.getUserMedia);
+    const tieneSoporteUserMedia = () =>
+        !!(navigator.mediaDevices.getUserMedia);
+    if(typeof MediaRecorder === "undefined" || !tieneSoporteUserMedia()){
+        return alert("Su navegador no cumple con los requisitos, por favor actualice a un navegador más reciente");
+    }
 
-
-    if(typeof mediaRecorder === "undefined" || !tieneSoprteUsermedia()){
-        return alert("su navegador no cumple con los requisitos, favor actualice");
-
-    } 
     const $listaDeDispositivos = document.querySelector("#listaDeDispositivos"),
-    $duracion = document.querySelector("#aduracion"),
-    $btnComenzarGrabacion = document.querySelector("#btnComenzarGrabacion"),
-    $btnComenzarDetenerGrabacion = document.querySelector("#btnDetenerGrabacion");
-
+            $duracion = document.querySelector("#duracion"),
+            $btnComenzarGrabacion = document.querySelector("btnComenzarGrabacion"),
+            $btnDetenerGrabacion = document.querySelector("#btnDetenerGrabacion");
 
     const limpiarSelect = () => {
-        
+        for(let x = $listaDeDispositivos.options.length-1; x >= 0; x--){
+            $listaDeDispositivos.options.remove(x);
+        }
     }
+
+    const segundosATiempo = numeroDeSegundos => {
+        let horas = Math.floor(numeroDeSegundos/60/60);
+        numeroDeSegundos -= horas * 60 * 60;
+        
+        let minutos = Math.floor(numeroDeSegundos/60);
+        numeroDeSegundos -= minutos * 60;
+
+        numeroDeSegundos = parseInt(numeroDeSegundos);
+        if(horas<10) horas = "0"+horas;
+        if(minutos<10) minutos = "0"+minutos;
+        if(numeroDeSegundos<10) numer = "0"+numeroDeSegundos;
+
+        return `${horas}:${minutos}:${numeroDeSegundos}`;
+
+    };
+
+    let tiempoInicion,mediaRecorder,idIntervalo;
+    const refrescar = () =>{
+        $duracion.textContent = segundosATiempo( (Date.now() - tiempoInicion) / 1000);
+    }
+
+    const llenarLista = () =>{
+        navigator.mediaDevices.enumerateDevices().then(dispositivos => {
+            limpiarSelect();
+            dispositivos.forEach((dispositivos,indice)=>{
+                if(dispositivos.kind === "audioinput"){
+                    const $opcion = document.createElement("option");
+                    $opcion.text = dispositivos.label || `Dispositivo ${indice+1}`;
+                    $opcion.value = dispositivos.deviceId;
+                    $listaDeDispositivos.appendChild($opcion);
+                }
+            })
+        })
+    };
+
+    const comenzarAContar = () =>{
+        tiempoInicion = Date.now();
+        idIntervalo = setInterval(refrescar,500);
+    }
+
+    const comenzarAGrabar = () =>{
+        if(!$listaDeDispositivos.options.length) return alert("No hay dispositivos");
+        if(mediaRecorder) return alert("Ya se está grabando");
+
+        navigator.mediaDevices.getUserMedia({
+            audio:{
+                deviceId:$listaDeDispositivos.value,
+            }
+        }).then(stream =>{
+            mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder.start();
+            comenzarAContar();
+            const fragmentosDeAudio = [];
+            mediaRecorder.addEventListener("dataavailable",evento =>{
+                fragmentosDeAudio.push(evento.data);
+            });
+            mediaRecorder.addEventListener("stop", ()=>{
+                stream.getTracks().forEach(track=>track.stop());
+                detenerConteo();
+                const blobAudio = new Blob(fragmentosDeAudio);
+                const urlParaDescargar = URL.createObjectURL(blobAudio);
+                let a = document.createElement("a");
+                document.body.appendChild(a);
+                a.style = "display:none";
+                a.href = urlParaDescargar;
+                a.download = "Sebastian.ufg.webm";
+                a.click();
+                window.URL.revokeObjectURL(urlParaDescargar);
+            });
+        }).catch(error => {
+            console.log(error);
+        });
+    };
+
+    const detenerConteo = () =>{
+        clearInterval(idIntervalo);
+        tiempoInicion =  null;
+        $duracion.textContent = "";
+    }
+
+    const detenerGrabacion = () =>{
+        if(!mediaRecorder) return alert("No se está grabando");
+        mediaRecorder.stop();
+        mediaRecorder = null;
+    }
+
+    $btnComenzarGrabacion-addEventListener("click", comenzarAGrabar);
+    $btnDetenerGrabacion.addEventListener("click",detenerGrabacion);
+
+    llenarLista();
 }
+document.addEventListener("DOMContentLoaded",init);
